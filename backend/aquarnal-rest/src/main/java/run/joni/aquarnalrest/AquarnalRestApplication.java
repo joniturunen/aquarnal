9d78417b96f674db755a4d4a018edcb7b5fdc4ab
Joni Turunen <joni.turunen@triplan.fi>
2022-05-18 12:58:04 +0300

    task: Create springboot project for Aquarnal REST

---
+++ backend/aquarnal-rest/src/main/java/run/joni/aquarnalrest/AquarnalRestApplication.java
@@ -0,0 +1,13 @@
+package run.joni.aquarnalrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquarnalRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquarnalRestApplication.class, args);
+	}
+
+}
---
+++ backend/aquarnal-rest/src/test/java/run/joni/aquarnalrest/AquarnalRestApplicationTests.java
@@ -0,0 +1,13 @@
+package run.joni.aquarnalrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquarnalRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
