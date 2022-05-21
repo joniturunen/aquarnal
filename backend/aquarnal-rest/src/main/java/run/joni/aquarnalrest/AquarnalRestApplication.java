32afe75965c05ee0b763d8107fdd2a2707ef76c4
Joni Turunen <joni.turunen@triplan.fi>
2022-05-21 12:23:06 +0300

    task: add basic list return for prototyping

--- backend/aquarnal-rest/src/main/java/run/joni/aquarnalrest/AquarnalRestApplication.java
+++ backend/aquarnal-rest/src/main/java/run/joni/aquarnalrest/AquarnalRestApplication.java
@@ -2,12 +2,21 @@ package run.joni.aquarnalrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class AquarnalRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AquarnalRestApplication.class, args);
 	}
 
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World");
+	}
 }
